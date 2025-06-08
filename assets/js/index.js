7f664e773790ecf241fe5351ab35cbda41a5ca89
Wendel Santos <67715702+dellstark@users.noreply.github.com>
2025-06-08 19:52:03 -0300

    Criando classes com JS
    Criando a classe tipoHeroi onde é definido o tipo de herói e trás um resultado mostrando qual o heroi e tipo de poder que ele possui

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,18 @@
-console.log("Passei aqui");
+class tipoHeroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+        atacar(){
+            console.log(`O ${this.nome} atacou usando ${this.tipo}`);
+        }
+};
+
+
+let guerreiro = new tipoHeroi("Guerreiro", 34, "Força");
+let monge = new tipoHeroi("Monge", 40, "Magia");
+let mago = new tipoHeroi("Mago", 46, "Magia");
+let ninja = new tipoHeroi("Ninja", 30, "Churiken");
+
+mago.atacar();
